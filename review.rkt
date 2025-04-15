f524738911ad0ee623cf511c558181aa1da73ec7
Bogdan Popa <bogdan@defn.io>
2025-04-15 08:16:40 +0300

    support definer-struct-define

--- review.rkt
+++ review.rkt
@@ -9,10 +9,34 @@
  should-review-syntax?
  review-syntax)
 
+(define struct-definers
+  (make-hasheq))
+
+(define (track-definer! id-stx struct-id-stx)
+  (hash-set! struct-definers (syntax->datum id-stx) struct-id-stx))
+
+(define (definer-id? id-stx)
+  (hash-has-key? struct-definers (syntax->datum id-stx)))
+
+(define (definer-ref id-stx)
+  (hash-ref struct-definers (syntax->datum id-stx)))
+
 (define (should-review-syntax? stx)
-  (syntax-case stx (struct-define)
-    [(struct-define . _rest) #t]
+  (syntax-parse stx
+    [({~datum define-struct-define} . _rest) #t]
+    [({~datum struct-define} . _rest) #t]
+    [(id _e) #:when (definer-id? #'id) #t]
     [_ #f]))
+
+(define-syntax-class define-struct-define-use
+  #:datum-literals (define-struct-define)
+  (pattern (define-struct-define definer-id:id struct-id:id)
+           #:do [(track-definer! #'definer-id #'struct-id)]))
+
+(define-syntax-class definer-use
+  (pattern (id e:expression)
+           #:when (definer-id? #'id)
+           #:do [(track-struct-usage (definer-ref #'id))]))
 
 (define-syntax-class struct-define-use
   #:datum-literals (struct-define)
@@ -21,5 +45,7 @@
 
 (define (review-syntax stx)
   (syntax-parse stx
+    [D:define-struct-define-use #'D]
     [d:struct-define-use #'d]
-    [_ (track-error stx "expected a deta schema or type definition")]))
+    [u:definer-use #'u]
+    [_ (track-error stx "expected a struct-define form")]))
